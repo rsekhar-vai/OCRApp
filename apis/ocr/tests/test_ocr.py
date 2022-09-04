cf399acc0e208ae77b7483612ea05731ff03fdf4
Raja-V <rsekhar.vai@gmail.com>
2022-09-04 23:21:51 +0530

    after first test_ocr testing complete

--- apis/ocr/tests/test_ocr.py
+++ apis/ocr/tests/test_ocr.py
@@ -1,5 +1,20 @@
+from fileinput import filename
+from fastapi import status, HTTPException  
+import platform
+import pdb
+from pathlib import Path
+from ocr.main import app
+from fastapi.testclient import TestClient
 import pytest
 
 
-def test_ocr():
-    assert 1 == 1
+@pytest.fixture()
+def os_type():
+    return platform.system() 
+
+def test_ocr(os_type):
+    client = TestClient(app)
+    testfile = Path(__file__).parent.parent.joinpath('testdata').joinpath('image_with_txt.png')
+    files = {"image" : testfile.open("rb")}
+    response =  client.post("/ocr", files=files)
+    assert response.status_code == status.HTTP_200_OK
